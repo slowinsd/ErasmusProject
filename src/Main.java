925b51fcd230621873185e50f9408ad294b188ac
slowinsd <dylan.slowinski@hotmail.fr>
2017-04-12 12:13:37 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-
+import maps.java.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("MDRR");
 		System.out.println("LOL");
 	}
 
